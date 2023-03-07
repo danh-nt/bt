daa584d7177d856060943513aaeb566a7460fff6
danh-nt <danh.nt2710@gmail.com>
2023-03-07 21:46:01 +0700

    1.24

---
+++ 1_24.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+struct HOPSUA
+{
+    char nhan[20];
+    int trongluong;
+    int ngay;
+};
+void nhap(struct HOPSUA *a)
+{
+    printf("nhap nhan hieu: ");
+    scanf("%s", &a->nhan);
+    printf("nhap trong luong: ");
+    scanf("%d", &a->trongluong);
+    printf("nhap ngay: ");
+    scanf("%d",&a->ngay);
+}
+void xuat(struct HOPSUA a)
+{
+    printf("nhan hieu: %s\ntrong luong: %d\n ngay: %d", a.nhan, a.trongluong, a.ngay);
+}
+int main()
+{
+    struct HOPSUA a;
+    nhap(&a);
+    xuat(a);
+    return 0;
+}
