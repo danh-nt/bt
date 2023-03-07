1fb36d9d168e930fc5c9935644de8813ad176da5
danh-nt <danh.nt2710@gmail.com>
2023-03-07 21:26:03 +0700

    1.4

---
+++ 1_4.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+struct diem3
+{
+    float x, y, z;
+};
+void nhap(struct diem3 *a)
+{
+    printf("nhap toa do x: ");
+    scanf("%f", &a->x);
+    printf("nhap toa do y: ");
+    scanf("%f", &a->y);
+    printf("nhap toa do z: ");
+    scanf("%f", &a->z);
+}
+void xuat(struct diem3 a)
+{
+    printf("(%f;%f;%f)",a.x, a.y, a.z);
+}
+int main()
+{
+    struct diem3 a;
+    nhap(&a);
+    xuat(a);
+    return 0;
+
+}
