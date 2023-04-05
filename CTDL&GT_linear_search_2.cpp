a7033a1b956ddb021b4576bcdf4b80b2b8d8e9a0
danh-nt <125142607+danh-nt@users.noreply.github.com>
2023-04-05 13:56:33 +0700

    Create CTDL&GT_linear_search_2.cpp

---
+++ CTDL&GT_linear_search_2.cpp
@@ -0,0 +1,50 @@
+// C code to linearly search x in arr[]. If x
+// is present then return its location, otherwise
+// return -1
+
+#include <stdio.h>
+#define MAX 100
+void Nhapmang(int arr[], int &n)
+{
+    do
+    {
+        printf("Nhap kich thuoc mang: ");
+        scanf("%d", &n);
+       
+        if(n<= 0 || n > MAX)
+        {
+            printf("Kich thuoc mang phai tu 1-100\n");
+        }
+    }while(n <= 0 || n > MAX);
+
+    for(int i=0; i<n; i++)
+    {
+        printf("Nhap a[%d]: ", i);
+        scanf("%d", &arr[i]);
+    }
+}
+int search(int arr[], int n, int x)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		if (arr[i] == x)
+			return i;
+	return -1;
+}
+
+// Driver code
+int main(void)
+{
+	int n,x,arr[MAX];
+	Nhapmang(arr,n);
+	printf("nhap x can tim: ");
+	scanf("%d",&x);
+	int N = sizeof(arr) / sizeof(arr[0]);
+
+	// Function call
+	int result = search(arr, n, x);
+	(result == -1)
+		? printf("Element is not present in array")
+		: printf("Element is present at index %d", result);
+	return 0;
+}
